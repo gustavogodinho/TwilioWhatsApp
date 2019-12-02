69e680db19f09d0780d83c00a13af6627440ba9c
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-12-02 18:28:00 -0300

    primeiro commit

--- TwilioAPIWhatsApp/Program.cs
+++ TwilioAPIWhatsApp/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
-namespace TwilioAPIWhatsApp
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        // Find your Account Sid and Token at twilio.com/console
+        // DANGER! This is insecure. See http://twil.io/secure
+        const string accountSid = "AC6a2a8a255366902772b2dbf438bba5d7";
+        const string authToken = "b1ef326d8273c186a1308ac568bdcedf";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            body: "ai sim meu patrao!",
+            from: new Twilio.Types.PhoneNumber("whatsapp:+14155238886"),
+            to: new Twilio.Types.PhoneNumber("whatsapp:+5511991164957")
+        );
+
+        Console.WriteLine(message.Sid);
     }
 }
