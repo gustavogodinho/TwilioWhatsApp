939c4fb9426f4ef800f2c0b16a8426a58f297a5b
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-12-03 10:26:02 -0300

    Thread.Sleep(10000)

--- TwilioAPIWhatsApp/Program.cs
+++ TwilioAPIWhatsApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
@@ -21,17 +22,56 @@ class Program
             var message = MessageResource.Create(
                 body: $"Image Twilio Sandbox for WhatsApp {i}",
                 from: new Twilio.Types.PhoneNumber("whatsapp:+14155238886"),
-                to: new Twilio.Types.PhoneNumber("whatsapp:+5511954580057"),
-                mediaUrl : new List<Uri> { new Uri("images/img.png") }
+                to: new Twilio.Types.PhoneNumber("whatsapp:+5511991164957"),
+                mediaUrl : new List<Uri> { new Uri("https://raw.githubusercontent.com/gustavogodinho/TwilioWhatsApp/master/TwilioAPIWhatsApp/images/img.png") }
 
             );
 
             Console.WriteLine(message);
         }
 
-       
+        Console.WriteLine("Sleep 1");
+        Thread.Sleep(10000);
 
-        
+        for (int i = 0; i < 10; i++)
+        {
+            TwilioClient.Init(accountSid, authToken);
+
+
+
+            var message = MessageResource.Create(
+                body: $"Image Twilio Sandbox for WhatsApp {i}",
+                from: new Twilio.Types.PhoneNumber("whatsapp:+14155238886"),
+                to: new Twilio.Types.PhoneNumber("whatsapp:+5511954580057"),
+                mediaUrl: new List<Uri> { new Uri("https://raw.githubusercontent.com/gustavogodinho/TwilioWhatsApp/master/TwilioAPIWhatsApp/images/img.png") }
+
+            );
+
+            Console.WriteLine(message);
+        }
+
+        Console.WriteLine("Sleep 2");
+        Thread.Sleep(10000);
+
+        for (int i = 0; i < 10; i++)
+        {
+            TwilioClient.Init(accountSid, authToken);
+
+
+
+            var message = MessageResource.Create(
+                body: $"Image Twilio Sandbox for WhatsApp {i}",
+                from: new Twilio.Types.PhoneNumber("whatsapp:+14155238886"),
+                to: new Twilio.Types.PhoneNumber("whatsapp:+5511991164957"),
+                mediaUrl: new List<Uri> { new Uri("https://raw.githubusercontent.com/gustavogodinho/TwilioWhatsApp/master/TwilioAPIWhatsApp/images/img.png") }
+
+            );
+
+            Console.WriteLine(message);
+        }
+
+
+
         Console.ReadKey();
     }
 }
